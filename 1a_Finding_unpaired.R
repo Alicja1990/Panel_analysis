2d3f96d1ee45859e59d6f564ac7bc236483d4b8b
Alicja <puchatek_90@o2.pl>
2018-08-13 12:53:25 -0700

    - finding unmatched funds for 2017 and 2018

---
+++ 1a_Finding_unpaired.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Alicja/Documents/Doktorat/Rozprawa doktorska/Panel_analysis/Data")
+x <- read.csv("pd.profile.csv", as.is = T)
+
+a <- read.csv("data_analizy_18.csv", as.is = T)
+b <- read.csv2("stooq_17_18.csv", as.is = T)
